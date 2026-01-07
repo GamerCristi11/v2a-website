6ad94a95eb105badfdad75366d5b627aff3fd493
Code2Craft <168816818+GamerCristi11@users.noreply.github.com>
2026-01-07 16:02:18 +0200

    Initial commit

---
+++ public/otherfiles/1.js
@@ -0,0 +1,16 @@
+//This just prints some text in the console
+
+console.log(
+    "%cWelcome to THE CONSOLE!",
+    "background: black; color: white; padding: 10px; font-size: 20px;"
+);
+
+console.log(
+    "%cAlso thanks for visiting my website!",
+    "background: black; color: white; padding: 10px; font-size: 10px;"
+);
+
+console.log(
+    "%c:3",
+    "background: black; color: white; padding: 10px; font-size: 50px;"
+);
